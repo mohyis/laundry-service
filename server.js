526b4ab45a588052d4ea089d4d955b03f52f1903
mohyis <mohyis007@gmail.com>
2026-05-16 00:28:21 +0100

    refactor server code by requiring passport

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('dotenv').config()
 const mongoose = require('mongoose');
+const passport = require('passport')
 const app = express()
 const PORT = 5907
 
